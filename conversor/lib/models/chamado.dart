87a3efcfadbaa26ff241a53ed5a489152ec2c760
LuanMateusK <luankusma@outlook.com>
2022-08-20 17:53:18 -0300

    Atualização Repositorio

---
+++ conversor/lib/models/chamado.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class Chamado {
+  String? nome;
+  String? descricao;
+  DateTime? dia;
+  TimeOfDay? hora;
+  double? duracao;
+  String? tipo;
+  bool importante = false;
+  List<String> materiais = <String>[];
+
+
+
+}
